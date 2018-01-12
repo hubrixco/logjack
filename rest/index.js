9fff13b2a4a530dffc8ce0ce80bfd81e64e77bff
Michael Heath <m.heath66@gmail.com>
2018-01-11 22:53:56 -0300

    start of restifying

--- rest/index.js
+++ rest/index.js
@@ -1,5 +1,5 @@
 // set up the web server
-const http = require('http');
+/*const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -12,4 +12,20 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+});*/
+
+var restify = require('restify');
+
+/*function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}*/
+
+var server = restify.createServer();
+//server.get('/hello/:name', respond);
+//server.head('/hello/:name', respond);
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
 });
+

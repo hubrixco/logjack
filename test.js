32e23047515205dc13cb10cad1cb354eb1913aea
Michael Heath <m.heath66@gmail.com>
2018-02-06 17:54:02 -0300

    Added test for compressed logs

--- test.js
+++ test.js
@@ -43,14 +43,24 @@ describe("Logger colored", function () {
 // test file with text
 describe("Logger file", function () {
     it("outputs log to file at level: trace", function () {
-        var fileAppenderSettings = new FileAppenderSettings("test.log",10 * 1024,5,false,true);
-        //fileAppenderSettings.fileName = "new.log";
-
-        //hubrixLogger = logger.getLogger('file','text','trace',fileAppenderSettings);
+        var fileAppenderSettings = new FileAppenderSettings("testfile.log",10 * 1024,5,false,true);
         hubrixLogger = new HubrixLogger('file','text','trace',fileAppenderSettings).logger;
 
         hubrixLogger.info('this is just a test');
         hubrixLogger.error('of a custom appender');
         hubrixLogger.warn('that outputs to file');
+    });
+});
+
+// test file with compression
+describe("Logger file", function () {
+    it("outputs log to three compressed files at level: trace", function () {
+        var fileAppenderSettings = new FileAppenderSettings("testcompressedfile.log",50,3,true,false);
+        hubrixLogger = new HubrixLogger('file','text','trace',fileAppenderSettings).logger;
+
+        hubrixLogger.info('this is just a test');
+        hubrixLogger.error('of a custom appender');
+        hubrixLogger.warn('that outputs to a compressed file');
+        hubrixLogger.trace('which should create three compressed files.');
     });
 });

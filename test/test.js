8900e9086ea4c50fcbff92865005a10f25f7ee76
Michael Heath <m.heath66@gmail.com>
2018-02-16 16:13:36 -0300

    refactored to use configuration object

--- test/test.js
+++ test/test.js
@@ -1,16 +1,24 @@
 var assert = require('assert');
 
 // import classes
-var FileAppenderSettings = require("../appenders/fileAppenderSettings.js"); 
-var AppenderSettings = require("../appenders/appenderSettings.js"); 
 var JackLogger = require("../jackLogger.js"); 
+
+var config = {
+    appenders: {
+      out: { type: 'console', layout: { type: 'json'} }
+    },
+    categories: {
+      default: { appenders: ['out'], level: 'info' }
+    }
+  };
 
 //var jackLogger = new jackLogger('socket','basic','trace',null).logger;
 
 // test console with json
 describe("Logger JSON", function () {
     it("outputs log in JSON at level: trace", function () {
-        var jackLogger = new JackLogger('console','json','trace',null).logger;
+        //var jackLogger = new JackLogger('console','json','trace',null).logger;
+        var jackLogger = new JackLogger(config).logger;
         jackLogger.info('testing');
         jackLogger.info('this is just a test');
         jackLogger.error('of a custom appender');
@@ -18,12 +26,19 @@ describe("Logger JSON", function () {
     });
 });
 
-
-
 // test stdout with xml
+config = {
+    appenders: {
+      out: { type: 'console', layout: { type: 'xml'} }
+    },
+    categories: {
+      default: { appenders: ['out'], level: 'warn' }
+    }
+  };
+
 describe("Logger XML", function () {
     it("outputs log in XML at level: warn", function () {
-        jackLogger = new JackLogger('stdout','xml','warn',null).logger;
+        jackLogger = new JackLogger(config).logger;
         jackLogger.info('this is just a test');
         jackLogger.error('of a custom appender');
         jackLogger.warn('that outputs xml');
@@ -31,20 +46,39 @@ describe("Logger XML", function () {
 });
 
 // test console with text colored
+config = {
+    appenders: {
+      out: { type: 'console', layout: { type: 'colored'} }
+    },
+    categories: {
+      default: { appenders: ['out'], level: 'trace' }
+    }
+  };
+
 describe("Logger colored", function () {
     it("outputs log in color at level: trace", function () {
-        jackLogger = new JackLogger('console','colored','trace',null).logger;
+        jackLogger = new JackLogger(config).logger;
         jackLogger.info('this is just a test');
         jackLogger.error('of a basic layout');
         jackLogger.warn('that outputs colored');
     });
 });
 
+
+
 // test file with text
+config = {
+    appenders: {
+      out: { type: 'file', filename: 'testfile.log', maxLogSize: 10485760, backups: 5, compress: false }
+    },
+    categories: {
+      default: { appenders: ['out'], level: 'trace' }
+    }
+  };
+
 describe("Logger file", function () {
     it("outputs log to file at level: trace", function () {
-        var fileAppenderSettings = new FileAppenderSettings("testfile.log",10 * 1024,5,false,true);
-        jackLogger = new JackLogger('file','text','trace',fileAppenderSettings).logger;
+        jackLogger = new JackLogger(config).logger;
 
         jackLogger.info('this is just a test');
         jackLogger.error('of a custom appender');
@@ -53,10 +87,18 @@ describe("Logger file", function () {
 });
 
 // test file with compression
+config = {
+    appenders: {
+      out: { type: 'file', filename: 'testcompressedfile.log', maxLogSize: 50, backups: 3, compress: true }
+    },
+    categories: {
+      default: { appenders: ['out'], level: 'trace' }
+    }
+  };
+
 describe("Logger file", function () {
     it("outputs log to three compressed files at level: trace", function () {
-        var fileAppenderSettings = new FileAppenderSettings("testcompressedfile.log",50,3,true,false);
-        jackLogger = new JackLogger('file','text','trace',fileAppenderSettings).logger;
+        jackLogger = new JackLogger(config).logger;
 
         jackLogger.info('this is just a test');
         jackLogger.error('of a custom appender');

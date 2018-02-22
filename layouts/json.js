3c021679d3ea87fab41eb25a8330ae4451319419
Michael Heath <m.heath66@gmail.com>
2018-02-22 08:51:59 -0300

    fixed path error

--- layouts/json.js
+++ layouts/json.js
@@ -2,7 +2,7 @@ const log4js = require('../../log4js-node');
 
 function addLayout(){
     log4js.addLayout('json', config => function (logEvent) {
-        return JSON.stringify(logEvent) + config.separator;
+        return JSON.stringify(logEvent) + ',';
     });
 }
 
